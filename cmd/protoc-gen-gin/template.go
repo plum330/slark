4e2f25b6547e960011461f499aa9334fff335c28
LNG <longkuicheng@class100.com>
2023-02-05 20:54:21 +0800

    tpl & http client

--- cmd/protoc-gen-gin/template.go
+++ cmd/protoc-gen-gin/template.go
@@ -35,13 +35,13 @@ func _{{$svrType}}_{{.Name}}{{.Num}}_HTTP_Handler(srv {{$svrType}}HTTPServer) gi
 		newCtx := context.TODO()
 		err = ctx.ShouldBind(&in)
 		if err != nil {
-			err = errors.InternalServer(errors.FormatInvalid, errors.FormatInvalid)
+			err = errors.InternalServer(err.Error(), errors.FormatInvalid)
 			goto Label
 		}
 
 		err = in.ValidateAll()
 		if err != nil {
-			err = errors.BadRequest(errors.ParamValid, errors.ParamValid)
+			err = errors.BadRequest(err.Error(), errors.ParamValid)
 			goto Label
 		}
 
