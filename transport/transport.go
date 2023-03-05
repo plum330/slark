dcaebe5596cf58c52ddab0ad79286e091c4819c8
LNG <longkuicheng@class100.com>
2023-03-05 17:49:08 +0800

    init

--- transport/transport.go
+++ transport/transport.go
@@ -1,6 +1,13 @@
 package transport
 
-import "context"
+import (
+	"context"
+
+	_ "github.com/go-slark/slark/encoding/form"
+	_ "github.com/go-slark/slark/encoding/json"
+	_ "github.com/go-slark/slark/encoding/msgpack"
+	_ "github.com/go-slark/slark/encoding/proto"
+)
 
 type Server interface {
 	Start() error
